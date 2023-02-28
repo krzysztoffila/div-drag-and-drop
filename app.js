5e3713bd22bb359efc4b8f866ca39f057e1ddfc1
krzysztoffila <umowy.fila@gmail.com>
2023-02-28 12:54:15 +0100

    v1

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ div.style.top = `${divY}px`;
 
 let drawActive = false;
 
-div.addEventListener('mousedown', () => {
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
     div.style.backgroundColor = 'silver';
     drawActive = !drawActive;
+    insertDivX = e.offsetX
+    insertDivY = e.offsetY
+    // console.log(insertDivX, insertDivY)
 })
 div.addEventListener('mousemove', (event) => {
     if (drawActive) {
-        divX = event.clientX;
-        divY = event.clientY;
-        div.style.left = `${divX -50}px`;
-        div.style.top = `${divY-50}px`;
+        divX = event.clientX - insertDivX;
+        divY = event.clientY - insertDivY;
+        div.style.left = `${divX}px`;
+        div.style.top = `${divY}px`;
     }
 })
 div.addEventListener('mouseup', () => {
